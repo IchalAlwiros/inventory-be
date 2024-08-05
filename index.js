7dcef6cce484de41fe4ec76d5669f47346e66db1
IchalAlwiros <ichalwiradev@gmail.com>
2024-08-05 16:21:21 +0700

    initial

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ require("./src/routes")(app);
 // PORT
 const port = process.env.PORT || 3000;
 
-// Listen on `port` and 0.0.0.0
 app.listen(port, "0.0.0.0", function () {
   // ...
   console.log(`Listen port`);
